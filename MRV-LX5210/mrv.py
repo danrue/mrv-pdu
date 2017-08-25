76bcf7650c53f910317df624907ca9df55316a5c
Dan Rue <drue@therub.org>
2017-08-25 13:52:07 -0500

    Mostly works.. Does not work in tight loop

--- MRV-LX5210/mrv.py
+++ MRV-LX5210/mrv.py
@@ -1,23 +1,48 @@
+import sys
 import telnetlib
 import time
 
+class MRV(object):
 
-HOST = "10.0.0.80"
-user = "Admn"
-password = "admn"
+    def __init__(self, host, username="Admn", password="admn"):
+        self.tn = telnetlib.Telnet(host, timeout=2)
+        self.tn.set_debuglevel(999)
+        self.tn.read_until(b"Username: ", 2)
+        self.tn.write(username.encode('ascii') + b"\n")
+        self.tn.read_until(b"Password: ", 2)
+        self.tn.write(password.encode('ascii') + b"\n")
 
-tn = telnetlib.Telnet(HOST)
+    def reboot(self, port):
+        self.tn.read_until(b"LX: ", 2)
+        self.tn.write(b"off .a%d\n" % int(port))
+        time.sleep(2)
+        self.tn.read_until(b"LX: ", 2)
+        self.tn.write(b"on .a%d\n" % int(port))
+        time.sleep(2)
 
-tn.read_until(b"Username: ")
-tn.write(user.encode('ascii') + b"\n")
-if password:
-    tn.read_until(b"Password: ")
-    tn.write(password.encode('ascii') + b"\n")
+    def off(self, port):
+        self.tn.read_until(b"LX: ", 2)
+        self.tn.write(b"off .a%d\n" % int(port))
+        time.sleep(2)
 
-tn.write(b"off .a5\n")
-time.sleep(5)
-tn.write(b"on .a5\n")
-tn.write(b"exit\n")
-print(tn.read_all().decode('ascii'))
+    def on(self, port):
+        self.tn.read_until(b"LX: ", 2)
+        self.tn.write(b"on .a%d\n" % int(port))
+        time.sleep(2)
 
+    # This doesn't work yet.
+    #def status(self, port):
+    ##    #self.tn.read_until(b"LX: ")
+    #    print(self.tn.read_very_eager().decode('ascii'))
+    #    self.tn.write(b"status .a6\n")
+    #    print(self.tn.read_all().decode('ascii'))
 
+if __name__ == "__main__":
+    usage = "Usage: {} hostname port [reboot|off|on]".format(sys.argv[0])
+    try:
+        (prog, host, port, action) = sys.argv
+    except:
+        sys.exit(usage)
+
+    mrv = MRV(host)
+    getattr(mrv, action)(port)
